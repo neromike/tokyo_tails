a70025454b05ec22f6c8fb6013a82e41b295ea5f
Wayne Pereanu <github@pereanu.com>
2023-12-31 23:15:11 -0500

    First implementation of A*star pathfinding

--- test.py
+++ test.py
@@ -24,8 +24,8 @@ GRID_CELL_SIZE = 40  # Size of each grid cell in pixels
 GRID_WIDTH = SCREEN_WIDTH // GRID_CELL_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_CELL_SIZE
 def initialize_grid():
-    # Create a 2D array with all cells set to passable (True)
-    return [[True for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+    # Create a 2D array with all cells set to passable (False)
+    return [[False for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
 def mark_obstacles_on_grid():
     # Reset the grid
     grid = initialize_grid()
@@ -50,7 +50,7 @@ def mark_obstacles_on_grid():
         for x in range(top_left_cell[0], bottom_right_cell[0] + 1):
             for y in range(top_left_cell[1], bottom_right_cell[1] + 1):
                 if 0 <= x < GRID_WIDTH and 0 <= y < GRID_HEIGHT:
-                    grid[y][x] = False  # Mark cell as impassable
+                    grid[y][x] = True  # Mark cell as impassable
     return grid
 def draw_grid(passable_color=(0, 255, 0), impassable_color=(255, 0, 0)):
     for y, row in enumerate(grid):
@@ -67,6 +67,98 @@ def pixel_to_grid(position):
 grid = initialize_grid()
 
 
+
+class Node():
+    # A node class for A* Pathfinding
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+        self.g = 0
+        self.h = 0
+        self.f = 0
+    def __eq__(self, other):
+        return self.position == other.position
+def astar(maze, start, end):
+    # Returns a list of tuples as a path from the given start to the given end in the given maze
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
 
 
 
@@ -316,7 +408,10 @@ class NPC(Actor):
         # Get grid start and end positions
         cat_position = pixel_to_grid(self.position)
         food_position = pixel_to_grid(item_cat_food.position)
-        print(f'cat({cat_position[0]}, {cat_position[1]}) food({food_position[0]}, {food_position[1]})')
+        path = astar(grid, cat_position, food_position)
+        #print(f'cat({cat_position[0]}, {cat_position[1]}) food({food_position[0]}, {food_position[1]})')
+        #print(grid)
+        print(path)
 
     
     
